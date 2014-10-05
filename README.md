e3cac8de39d16aa35ce37e6cd652c1179789bfc6
Yanni Wen <yanni_wen@pmmsc.org>
2014-10-06 01:56:35 +0800

    init and v0.0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+# NodeBB Plugin SSO QQ
+
+NodeBB Plugin that allows users to login/register via their QQ account.
+
+## Installation
+
+    npm install nodebb-plugin-sso-qq
+
+Please config callback address to your nodebb url
---
+++ library.js
@@ -0,0 +1,138 @@
+(function(module) {
+    "use strict";
+
+    var User = module.parent.require('./user'),
+        db = module.parent.require('../src/database'),
+        meta = module.parent.require('./meta'),
+        passport = module.parent.require('passport'),
+        passportQQ = require('passport-qq').Strategy,
+        fs = module.parent.require('fs'),
+        path = module.parent.require('path');
+
+    var constants = Object.freeze({
+        'name': "qq",
+        'admin': {
+            'icon': 'fa-qq',
+            'route': '/qq'
+        }
+    });
+
+    var QQ = {};
+
+    QQ.getStrategy = function(strategies, callback) {
+        if (meta.config['social:qq:id'] && meta.config['social:qq:key']) {
+            passport.use(new passportQQ({
+                clientID: meta.config['social:qq:id'],
+                clientSecret: meta.config['social:qq:key'],
+                callbackURL: module.parent.require('nconf').get('url') + '/auth/qq/callback'
+            },function(token, tokenSecret, profile, done) {
+
+                    console.log(token);
+                    console.log(tokenSecret);
+                    console.log(profile);
+
+                var email = ''
+                if(profile.emails && profile.emails.length){
+                    email = profile.emails[0].value
+                }
+                var picture = profile.avatarUrl;
+                if(profile._json.figureurl_qq_1){
+                    picture = profile._json.figureurl_qq_1;
+                }
+                if(profile._json.figureurl_qq_2){
+                    picture = profile._json.figureurl_qq_2;
+                }
+                QQ.login(profile.id, profile.nickname, email, picture, function(err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+                    done(null, user);
+                });
+            }));
+
+            strategies.push({
+                name: 'qq',
+                url: '/auth/qq',
+                callbackURL: '/auth/qq/callback',
+                icon: 'qq',
+                scope: 'user:email'
+            });
+        }
+
+        callback(null, strategies);
+    };
+
+    QQ.login = function(qqID, username, email, picture, callback) {
+        console.log("qqID=" + qqID + ", username=" + username);
+        if (!email) {
+            email = username + '@users.noreply.qq.com';
+        }
+
+        QQ.getUidByqqID(qqID, function(err, uid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (uid) {
+                // Existing User
+                callback(null, {
+                    uid: uid
+                });
+            } else {
+                // New User
+                var success = function(uid) {
+                    User.setUserField(uid, 'qqid', qqID);
+                    db.setObjectField('qqid:openid', qqID, uid);
+                    callback(null, {
+                        uid: uid
+                    });
+                };
+
+                User.getUidByEmail(email, function(err, uid) {
+                    if (!uid) {
+                        User.create({username: username, email: email, picture:picture, uploadedpicture:picture}, function(err, uid) {
+                            if (err !== null) {
+                                callback(err);
+                            } else {
+                                success(uid);
+                            }
+                        });
+                    } else {
+                        success(uid); // Existing account -- merge
+                    }
+                });
+            }
+        });
+    };
+
+    QQ.getUidByqqID = function(qqID, callback) {
+        db.getObjectField('qqid:openid', qqID, function(err, uid) {
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, uid);
+            }
+        });
+    };
+
+    QQ.addMenuItem = function(custom_header, callback) {
+        custom_header.authentication.push({
+            "route": constants.admin.route,
+            "icon": constants.admin.icon,
+            "name": constants.name
+        });
+
+        callback(null, custom_header);
+    };
+
+    function renderAdmin(req, res, callback) {
+        res.render('sso/qq/admin', {});
+    }
+
+    QQ.init = function(app, middleware, controllers) {
+        app.get('/admin/qq', middleware.admin.buildHeader, renderAdmin);
+        app.get('/api/admin/qq', renderAdmin);
+    };
+
+    module.exports = QQ;
+}(module));
